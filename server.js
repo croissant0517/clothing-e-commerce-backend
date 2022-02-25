320397e25318fe0b32d8ea705ae8273bbb84ea71
croissant <wathyic@gmail.com>
2022-02-26 07:57:00 +0800

    fix bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 const corsOptions = {
-    origin: ["https://croissant0517.github.io/big-data-company-test/", "http://localhost:3002/big-data-company-test"],
+    origin: ["https://croissant0517.github.io", "http://localhost:3002/big-data-company-test"],
     credentials: true
 }
 app.use(cors(corsOptions));

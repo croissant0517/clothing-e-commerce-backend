93ab97e09384fa9e1767c1a0d65881315fefdab4
croissant <wathyic@gmail.com>
2022-02-26 00:02:40 +0800

    add search feature

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const fetch = require('node-fetch');
 
 if(process.env.NODE_ENV !== "production") {

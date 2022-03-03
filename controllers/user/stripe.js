63b4290ab5ca360484a0674ee3e88dffce3b472f
croissant <wathyic@gmail.com>
2022-03-03 12:37:27 +0800

    fix payment bug

--- controllers/user/stripe.js
+++ controllers/user/stripe.js
@@ -1,5 +1,5 @@
 // This is your test secret API key.
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")('sk_test_51JjUcqGBBygI9WOW3rA0DzaaDuE3zd5oHxrx2chxwS3hJOG2mHPv5xGRhZKhGeBOpalcBbaHFBAmoshw8iQUWErf00CQjwGTez');
 
 const handleCreatePaymentIntent = async (req, res) => {
     const { total } = req.body;

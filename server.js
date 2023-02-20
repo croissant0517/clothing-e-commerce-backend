ff08483b2e2efb3a4f50924de039b405b3759ab4
croissant <wathyic@gmail.com>
2023-02-21 02:41:31 +0800

    add comment on process.env.NODE_ENV

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-if(process.env.NODE_ENV !== "production") {  
-    require("dotenv").config()
+// Node.js applications now default to NODE_ENV=production in Heroku
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const signin = require("./controllers/admin/signin");
@@ -20,68 +21,129 @@ const stripe = require("./controllers/user/stripe");
 const search = require("./controllers/user/search");
 const auth = require("./middleware/auth");
 
-const { initializeApp } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
 const db = getFirestore();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
-const knex = require('knex')({
-    client: 'pg',
-    connection: (process.env.NODE_ENV !== "production") ? {
-      host : '127.0.0.1',
-      port : 5432,
-      database : 'overfit'
-    }
-    :
-    {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
+const knex = require("knex")({
+  client: "pg",
+  connection:
+    process.env.NODE_ENV !== "production"
+      ? {
+          host: "127.0.0.1",
+          port: 5432,
+          database: "overfit",
         }
-    }
+      : {
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
 });
 
 app.get("/", (req, res) => {
-    res.send("It is working!")
-})
+  res.send("It is working!");
+});
 
-app.post("/admin/signin", (req, res) => {signin.signinAuthentication(req, res, knex)});
+app.post("/admin/signin", (req, res) => {
+  signin.signinAuthentication(req, res, knex);
+});
 
+app.get("/admin/user", auth.requireAuthLevelTwo, (req, res) => {
+  users.handleGetAllUsers(req, res);
+});
+app.delete("/admin/user/delete", auth.requireAuthLevelOne, (req, res) => {
+  users.handleDeleteUser(req, res, db);
+});
 
-app.get("/admin/user", auth.requireAuthLevelTwo, (req, res) => {users.handleGetAllUsers(req, res)});
-app.delete("/admin/user/delete", auth.requireAuthLevelOne, (req, res) => {users.handleDeleteUser(req, res, db)});
+app.get("/admin/collections", (req, res) => {
+  collection.handleGetAllCollection(req, res, db);
+});
+app.post("/admin/collections/add", auth.requireAuthLevelOne, (req, res) => {
+  collection.handleAddCollection(req, res, db);
+});
+app.put("/admin/collections/update", auth.requireAuthLevelOne, (req, res) => {
+  collection.handleUpdateCollection(req, res, db);
+});
+app.delete(
+  "/admin/collections/delete",
+  auth.requireAuthLevelOne,
+  (req, res) => {
+    collection.handleDeleteCollection(req, res, db);
+  }
+);
 
-app.get("/admin/collections", (req, res) => {collection.handleGetAllCollection(req, res, db)});
-app.post("/admin/collections/add", auth.requireAuthLevelOne, (req, res) => {collection.handleAddCollection(req, res, db)});
-app.put("/admin/collections/update", auth.requireAuthLevelOne, (req, res) => {collection.handleUpdateCollection(req, res, db)});
-app.delete("/admin/collections/delete", auth.requireAuthLevelOne, (req, res) => {collection.handleDeleteCollection(req, res, db)});
+app.post(
+  "/admin/collections/item/add",
+  auth.requireAuthLevelOne,
+  (req, res) => {
+    collection.handleAddItem(req, res, db);
+  }
+);
+app.put(
+  "/admin/collections/item/update",
+  auth.requireAuthLevelOne,
+  (req, res) => {
+    collection.handleUpdateItem(req, res, db);
+  }
+);
+app.delete(
+  "/admin/collections/item/delete",
+  auth.requireAuthLevelOne,
+  (req, res) => {
+    collection.handleDeleteItem(req, res, db);
+  }
+);
 
-app.post("/admin/collections/item/add", auth.requireAuthLevelOne, (req, res) => {collection.handleAddItem(req, res, db)});
-app.put("/admin/collections/item/update", auth.requireAuthLevelOne, (req, res) => {collection.handleUpdateItem(req, res, db)});
-app.delete("/admin/collections/item/delete", auth.requireAuthLevelOne, (req, res) => {collection.handleDeleteItem(req, res, db)});
+app.get("/admin/orders", auth.requireAuthLevelTwo, (req, res) => {
+  order.handleGetAllOrders(req, res, db);
+});
+app.post("/admin/orders/delete", auth.requireAuthLevelOne, (req, res) => {
+  order.handleDeleteOrder(req, res, db);
+});
 
-app.get("/admin/orders", auth.requireAuthLevelTwo, (req, res) => {order.handleGetAllOrders(req, res, db)});
-app.post("/admin/orders/delete", auth.requireAuthLevelOne, (req, res) => {order.handleDeleteOrder(req, res, db)});
+app.get("/admin/sliders", (req, res) => {
+  slider.handleGetAllSliders(req, res, db);
+});
+app.post("/admin/sliders/add", auth.requireAuthLevelOne, (req, res) => {
+  slider.handleAddSlider(req, res, db);
+});
+app.put("/admin/sliders/update", auth.requireAuthLevelOne, (req, res) => {
+  slider.handleUpdateSlider(req, res, db);
+});
+app.delete("/admin/sliders/delete", auth.requireAuthLevelOne, (req, res) => {
+  slider.handleDeleteSlider(req, res, db);
+});
 
-app.get("/admin/sliders", (req, res) => {slider.handleGetAllSliders(req, res, db)});
-app.post("/admin/sliders/add", auth.requireAuthLevelOne, (req, res) => {slider.handleAddSlider(req, res, db)});
-app.put("/admin/sliders/update", auth.requireAuthLevelOne, (req, res) => {slider.handleUpdateSlider(req, res, db)});
-app.delete("/admin/sliders/delete", auth.requireAuthLevelOne, (req, res) => {slider.handleDeleteSlider(req, res, db)});
+app.get("/admin/activity", (req, res) => {
+  activity.handleGetActivity(req, res, db);
+});
+app.post("/admin/activity/add", auth.requireAuthLevelOne, (req, res) => {
+  activity.handleAddActivity(req, res, db);
+});
+app.put("/admin/activity/update", auth.requireAuthLevelOne, (req, res) => {
+  activity.handleUpdateActivity(req, res, db);
+});
+app.delete("/admin/activity/delete", auth.requireAuthLevelOne, (req, res) => {
+  activity.handleDeleteActivity(req, res, db);
+});
 
-app.get("/admin/activity", (req, res) => {activity.handleGetActivity(req, res, db)});
-app.post("/admin/activity/add", auth.requireAuthLevelOne, (req, res) => {activity.handleAddActivity(req, res, db)});
-app.put("/admin/activity/update", auth.requireAuthLevelOne, (req, res) => {activity.handleUpdateActivity(req, res, db)});
-app.delete("/admin/activity/delete", auth.requireAuthLevelOne, (req, res) => {activity.handleDeleteActivity(req, res, db)});
-
-app.post("/create-payment-intent", (req, res) => {stripe.handleCreatePaymentIntent(req, res)});
-app.get("/search", (req, res) => {search.handleUserSearch(req, res, db)});
-
-
+app.post("/create-payment-intent", (req, res) => {
+  stripe.handleCreatePaymentIntent(req, res);
+});
+app.get("/search", (req, res) => {
+  search.handleUserSearch(req, res, db);
+});
 
 // webhook
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
     let event = request.body;
 
     // Only verify the event if you have an endpoint secret defined.
@@ -103,25 +165,28 @@ app.post('/webhook', express.raw({type: 'application/json'}), (request, response
 
     // Handle the event
     switch (event.type) {
-        case 'payment_intent.succeeded':
-            const paymentIntent = event.data.object;
-            console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-            console.log(paymentIntent);
-            // Then define and call a method to handle the successful payment intent.
-            // handlePaymentIntentSucceeded(paymentIntent);
-            
-            break;
-        case 'payment_method.attached':
-            const paymentMethod = event.data.object;
-            // Then define and call a method to handle the successful attachment of a PaymentMethod.
-            // handlePaymentMethodAttached(paymentMethod);
-            break;
-        default:
-            // Unexpected event type
-            console.log(`Unhandled event type ${event.type}.`);
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        console.log(
+          `PaymentIntent for ${paymentIntent.amount} was successful!`
+        );
+        console.log(paymentIntent);
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+
+        break;
+      case "payment_method.attached":
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
     }
     // Return a 200 response to acknowledge receipt of the event
     response.send();
-});
+  }
+);
 
 app.listen(port, () => console.log("Node server listening on port " + port));
